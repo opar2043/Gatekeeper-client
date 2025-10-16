811c7dc57a9bec4a169205aeb7eefd19be5c19fd
Rimel <rezwanulrimels@gmail.com>
2025-10-16 13:09:52 +0600

    added new section

--- src/Components/Root/Home.jsx
+++ src/Components/Root/Home.jsx
@@ -1,11 +1,16 @@
 import Banner from '../Home/Banner'
 import Choose from '../Choose/Choose'
+import LandingFull from '../LandingFull/LandingFull'
+
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Choose></Choose>
+      <LandingFull></LandingFull>
+      
+
     </div>
   )
 }
